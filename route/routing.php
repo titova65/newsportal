9ee4621cf751e4ad36d59e69227fceead68c4825
dur1m4r <vladimirtiths@gmail.com>
2021-04-23 19:17:32 +0300

    ctage three

--- route/routing.php
+++ route/routing.php
@@ -27,6 +27,12 @@ elseif($path == 'news' and isset($_GET['id'])) {
 
 }
 
+elseif($path == 'insertcomment' and isset($_GET['comment'],$_GET['id'])) {
+	$response = Controller::InsertComment($_GET['comment'],$_GET['id']);
+
+}
+
+
 else{
 
 	$response = Controller::error404();
